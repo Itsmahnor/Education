39b9af9d28c8f49ebb16eb900e1c31426aa3abcd
itsmahnoor <mahnoorsarfraz23@gmail.com>
2024-12-23 12:25:55 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./Pages/Home";
+import './App.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+  return (
+   <>
+   <Home />
+   </>
+  )
+}
